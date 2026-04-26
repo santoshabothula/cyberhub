301a6501e89e191070589e2829d128469f4d49e2
santosh <santosh.abothula@gmail.com>
2026-04-26 08:50:21 +0530

    api naming conversion issues fixed

---
+++ src/main/java/com/sg/cyberhub/config/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package com.sg.cyberhub.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.enable(DeserializationFeature.USE_LONG_FOR_INTS);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+
+    @Bean
+    public RestClient restClient(ObjectMapper objectMapper) {
+        return RestClient.builder()
+                .messageConverters(converters -> converters.add(new MappingJackson2HttpMessageConverter(objectMapper)))
+                .build();
+    }
+}
